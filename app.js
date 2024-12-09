ece05fa2b971bd87c747e07c799d0e85512c9bb5
Reemas5 <sameerkhan570786@gmail.com>
2024-12-09 17:44:59 +0530

    first commit

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
 const http = require('http');
+const fs = require('fs');
 const server  = http.createServer((req,res)=>{
-    console.log("server is created");
-    
-    res.setHeader('Content-Type','text/html')
-    if(req.url =='/Home'){
-        res.end("<h2>Welcome home</h2>")
-    }
-    if(req.url =='/About'){
-        res.end("<h2>Welcome to About Us</h2>")
-    }
-    if(req.url =='/Node'){
-        res.end("<h2>Welcome to my Node.Js project</h2>")
+console.log("server is created");
+const url = req.url;
+const method = req.method;
+if (req.url==='/'){
+    res.setHeader('Content-Type','text/html');
+    res.end(
+        `<form action="/message" method="POST">
+            <label>Name:</label>
+            <input type="text" name="username"></input>
+            <button type="submit">Add</button>
+        </form>`
+        )
+} else {
+    if (req.url==='/message'){
+        res.setHeader('Content-Type','text/html');
+        let datachunks =[]
+        req.on('data',(chunks)=>{
+            console.log(chunks);
+            datachunks.push(chunks)
+            
+        })
+        req.on('end',()=>{
+            let combinedbuffer = Buffer.concat(datachunks);
+            console.log(combinedbuffer.toString())
+            let value = combinedbuffer.toString()
+            let formdata = value.toString().split("=")[1]
+            fs.writeFile('formdata.txt',formdata,(err)=>{
+                    res.statusCode= 302
+                    res.setHeader('Location','/')
+                    res.end()
+            })
+        })
     }
     else{
-        res.end("<h2>Page not Found</h2>")
-    }
+        if(req.url ==='/read'){
+            fs.readFile('formdata.txt',(err,data)=>{
+                console.log(data.toString())
+                res.end(`<h1>${data.toString()}</h1>`)
+            })
+        }
 
+    }
+} 
 
 })
 
-let port = 4000
+
+
+let port = 3000
 server.listen(port,()=>{
     console.log('server is running')
 })
